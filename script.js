85f4c6398435ba1c6ae69406ede6811cab5bca46
Manuel <manuelpazlorenzo@gmail.com>
2023-09-19 15:26:25 -0300

    realizo cambios en HTML y JS

--- script.js
+++ script.js
@@ -1,82 +1,84 @@
-const historial = [];   // Declaro un array para almacenar el historial de juegos.
+const historial = [];
 
-function bienvenida() {    // Función de bienvenida que muestra un mensaje de inicio.
-  alert("¡Vamos a jugar a los dados!");  
-}
+// Obtener referencias a elementos del DOM
+const inputForm = document.getElementById("inputForm");
+const nombreJugador1 = document.getElementById("nombreJugador1");
+const nombreJugador2 = document.getElementById("nombreJugador2");
+const resultadoDiv = document.getElementById("resultado");
+const historialDiv = document.getElementById("historial");
 
-bienvenida(); // Llamo a la función de bienvenida para comenzar el juego.
+cargarHistorial();// Carga el historial desde LocalStorage al inicio
 
-function juego() { // Función principal del juego.
-  const jugador1 = { // Creo objetos para representar a los dos jugadores.
-    nombre: prompt("Ingresa el nombre del Jugador 1:"),
-    resultado: 0, // Acá se almacenará el resultado del dado para el jugador, lo mismo suecede con el jugador 2.
+function juego(event) {
+  event.preventDefault(); // Evitar que el formulario se envíe
+
+  const jugador1 = {
+    nombre: nombreJugador1.value,
+    resultado: tirarDado(),
   };
 
   const jugador2 = {
-    nombre: prompt("Ingresa el nombre del Jugador 2:"),
-    resultado: 0, 
+    nombre: nombreJugador2.value,
+    resultado: tirarDado(),
   };
 
-  function tirarDado() { // Función para lanzar un dado y obtener un número aleatorio del 1 al 6.
-    return Math.floor(Math.random() * 6) + 1;
-  }
-
-  function determinarGanador() { // Función para determinar el ganador del juego y actualizar el historial.
-    jugador2.resultado = tirarDado();
-    jugador1.resultado = tirarDado();  // Cada jugador tira un dado y se almacena su resultado.
-
-    alert(jugador1.nombre + " lanzó un dado y obtuvo un " + jugador1.resultado); // Se muestra el resultado de los "lanzamientos" en un mensaje de alerta.
-    alert(jugador2.nombre + " lanzó un dado y obtuvo un " + jugador2.resultado);
-
-    if (jugador1.resultado > jugador2.resultado) { // comparo el resultado de los jugadores para determinar al ganador.
-      historial.push({ ganador: jugador1.nombre });
-      alert(jugador1.nombre + " gana!");
-    } else if (jugador2.resultado > jugador1.resultado) {
-      historial.push({ ganador: jugador2.nombre });
-      alert(jugador2.nombre + " gana!");
-    } else {
-      historial.push({ ganador: "Empate" });
-      alert("Es un empate.");
-    }
-  }
-
-  determinarGanador(); // Llamo a la función para determinar el ganador del juego.
-}
-
-function reiniciarJuego() { // Función para reiniciar el juego.
-  let jugarDeNuevo;
-
-  do {  // Bucle que permite volver a jugar mientras el usuario lo desee.
-    juego(); 
-    jugarDeNuevo = prompt("¿Querés volver a jugar? (Si/No)").toLowerCase(); // Se le pregunta al usuario si desea volver a jugar
-
-    if (jugarDeNuevo === "no") { // Si la respuesta es "no" se le pregunta si quiere buscar un resultado en el historial
-      const buscar = prompt("¿Deseas buscar un resultado en el historial? (Si/No)").toLowerCase();
-      if (buscar === "si") { 
-        buscarEnHistorial();
-      }
-    }
-  } while (jugarDeNuevo === "si"); //si la respuesta es "si" se reinicia el simulador 
-}
-
-function buscarEnHistorial() { // Función para buscar resultados en el historial.
-  const busqueda = prompt("Ingresa el nombre que deseas buscar:"); 
-
-  const resultadosCoincidentes = historial.filter((resultado) => // Filtro el historial para encontrar resultados coincidentes.
-    resultado.ganador.toLowerCase().includes(busqueda.toLowerCase())
-  );
-
-  if (resultadosCoincidentes.length === 0) { // Muestro los resultados encontrados o un mensaje si no se encontraron coincidencias.
-    alert("No se encontraron juegos con ese nombre.");
+  let ganador;
+  if (jugador1.resultado > jugador2.resultado) {
+    ganador = jugador1.nombre;
+  } else if (jugador2.resultado > jugador1.resultado) {
+    ganador = jugador2.nombre;
   } else {
-    const historialFormateado = resultadosCoincidentes.map((resultado) => resultado.ganador).join('\n'); // Formateo y muestro los resultados encontrados.
-    alert("Historial\n" + historialFormateado);
+    ganador = "Empate";
+  }
+
+  const resultadoHTML = `
+    <p>${jugador1.nombre} lanzó un dado y obtuvo un ${jugador1.resultado}</p>
+    <p>${jugador2.nombre} lanzó un dado y obtuvo un ${jugador2.resultado}</p>
+    <p>${ganador} gana!</p>
+  `;
+
+  resultadoDiv.innerHTML = resultadoHTML;
+
+  historial.push({
+    ganador,
+    jugador1: jugador1.resultado,
+    jugador2: jugador2.resultado,
+  });
+
+  guardarHistorial();
+
+  mostrarHistorial();
+}
+
+function tirarDado() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function mostrarHistorial() {
+  const historialHTML = historial
+    .map(
+      (resultado, index) => `
+    <p>Partida ${index + 1}: ${resultado.jugador1} vs ${
+        resultado.jugador2
+      } - Ganador: ${resultado.ganador}</p>
+  `
+    )
+    .join("");
+
+  historialDiv.innerHTML = `<h2>Historial</h2>${historialHTML}`;
+}
+
+function guardarHistorial() {
+  localStorage.setItem("historial", JSON.stringify(historial));
+}
+
+function cargarHistorial() {
+  const historialGuardado = localStorage.getItem("historial");
+
+  if (historialGuardado) {
+    historial = JSON.parse(historialGuardado);
+    mostrarHistorial();
   }
 }
 
-function cierreDelSimulador() { // Función para mostrar el historial al final del juego.
-  alert("Historial de ganadores:\n" + historial.map((resultado) => resultado.ganador).join('\n') + "\nGracias por jugar. ¡Hasta luego!");
-}
-
-reiniciarJuego(); 
-cierreDelSimulador();
+inputForm.addEventListener("submit", juego);
